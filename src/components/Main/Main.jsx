491ff660b5420314a1c4f2debc5e0562b12d9249
Emma Hardin <emma@Emmas-MacBook-Pro-2.local>
2021-12-08 12:24:37 -0800

    adding and wiring up components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+import Card from '../Card/Card';
+
+export default function Main() {
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+    </>
+  );
+}
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Search.css';
+
+export default function Search() {
+  return <h2>This is a search.</h2>;
+}
