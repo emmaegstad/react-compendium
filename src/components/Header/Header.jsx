4df98dde88f4e2a4deaa16a59a98db07bf155229
Emma Hardin <emma@Emmas-MacBook-Pro-2.local>
2021-12-09 10:26:17 -0800

    adding new logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import HeaderImg from '../../HeaderImg.png';
 
 export default function Header() {
   return (
     <div className="Header">
-      <h1>My Pokédex</h1>
+      {/* <h1>My Pokédex</h1> */}
+      <img className="header-img" src={HeaderImg} alt="pokedex logo" />
     </div>
   );
 }
